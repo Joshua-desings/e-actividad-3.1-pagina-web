0bada3eb8837e38aa21927e22d68df6d5178d856
Diego <107651797+1Skiy@users.noreply.github.com>
2023-11-20 19:32:47 -0400

    archivo de configuracion de la base de datos
    Con el codigo para crear las tablas del formulario

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+// Recuerda personalizar estos datos 
+$host = "localhost";
+$usuario = "root";
+$contrasena = "Fy4wi3uKvkXVXYKW";
+$base_de_datos = "formulario";
+
+/* Recuerda que la tabla debe llamarse datos_formulario esta es la estructura:
+
+    CREATE TABLE nombre_de_la_tabla (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nombre_completo VARCHAR(255) NOT NULL,
+        correo_electronico VARCHAR(255) NOT NULL,
+        ciudad VARCHAR(100) NOT NULL,
+        telefono VARCHAR(20) NOT NULL,
+        comentarios TEXT,
+        fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );    
+*/
+?>
+
